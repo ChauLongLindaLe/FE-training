1018ea45473c2d3c381d7bbaf5941ccb3837fffd
LE <887680@cognizant.com>
2021-02-23 20:19:34 +0000

    use regex to test string if not send error message

--- JavaScript Matrix/index.js
+++ JavaScript Matrix/index.js
@@ -9,8 +9,12 @@ var userInput = []
 
 rl.question('Input a string of nine unique numbers ', (answer) => {
   // take string of numbers and turn into array of numbers
-  userInput = answer.split('').map(Number);
-  console.log(userInput);
-
+    let pattern = /^[1-9]{9}$/;
+    if (pattern.test(answer)) {
+      userInput = answer.split('').map(Number);
+      console.log(userInput);
+    } else{
+      console.log('Please enter a 9 digit string that uses the numbers 1-9 once each');
+    }
   rl.close();
 });
