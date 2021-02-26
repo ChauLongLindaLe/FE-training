313bd6dc185136e9a3f720035cbdb2f2ddcde79d
LE <887680@cognizant.com>
2021-02-26 13:20:49 +0000

    added matrix

--- JavaScript Matrix/index.js
+++ JavaScript Matrix/index.js
@@ -5,16 +5,25 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-var userInput = []
+
 
 rl.question('Input a string of nine unique numbers ', (answer) => {
   // take string of numbers and turn into array of numbers
-    let pattern = /^[1-9]{9}$/;
-    if (pattern.test(answer)) {
-      userInput = answer.split('').map(Number);
-      console.log(userInput);
+  var userInput = []
+  let pattern = /^[1-9]{9}$/;
+  if (pattern.test(answer)) {
+    userInput = answer.split('').map(Number);
+    // turn into [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    matrixify(userInput);
     } else{
       console.log('Please enter a 9 digit string that uses the numbers 1-9 once each');
     }
-  rl.close();
-});
+    rl.close();
+  });
+
+  const matrixify = function(userInputArr){
+     matrix = userInputArr.reduce((rows, key, index) => (index % 3 == 0 ? rows.push([key]) 
+      : rows[rows.length-1].push(key)) && rows, []);
+      return console.log(matrix)
+  }
+  
