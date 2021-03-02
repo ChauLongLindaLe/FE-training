c35be8e7061566392dbfb040a839d0c4fc79990b
LE <887680@cognizant.com>
2021-03-02 14:56:37 +0000

    put all sums into one array

--- JavaScript Matrix/index.js
+++ JavaScript Matrix/index.js
@@ -5,7 +5,31 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+let matrix = []
+let sums = []
 
+const matrixify = function(userInputArr){
+  matrix = userInputArr.reduce((rows, key, index) => (index % 3 == 0 ? rows.push([key]) 
+  : rows[rows.length-1].push(key)) && rows, []);
+  matrixSumRows(matrix);
+  matrixSumDiagonals(matrix);
+   return console.log(sums);
+}
+
+const matrixSumDiagonals = function(matrix) {
+    let diagonal1 = 0, diagonal2 = 0;
+    for (let row = 0; row < matrix.length; row++) {
+        diagonal1 += matrix[row][row];
+        diagonal2 += matrix[row][matrix.length - row - 1];
+    }
+    return sums.push(diagonal1, diagonal2);
+}
+
+
+const matrixSumRows = function(matrix){
+    return matrix.map(r => sums.push(r.reduce((a, b) => a + b)));
+    
+ }
 
 rl.question('Input a string of nine unique numbers ', (answer) => {
   // take string of numbers and turn into array of numbers
@@ -21,9 +45,3 @@ rl.question('Input a string of nine unique numbers ', (answer) => {
     rl.close();
   });
 
-  const matrixify = function(userInputArr){
-     matrix = userInputArr.reduce((rows, key, index) => (index % 3 == 0 ? rows.push([key]) 
-      : rows[rows.length-1].push(key)) && rows, []);
-      return console.log(matrix)
-  }
-  
