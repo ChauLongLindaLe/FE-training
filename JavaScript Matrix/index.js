82950b4ca8799672aa2c9db09ec626f7254f6bc4
LE <887680@cognizant.com>
2021-03-02 19:39:36 +0000

    made matrix and remembered there needed to be column sums and added winner calculation

--- JavaScript Matrix/index.js
+++ JavaScript Matrix/index.js
@@ -6,30 +6,39 @@ const rl = readline.createInterface({
 });
 
 let matrix = []
-let sums = []
+let rowSums = []
+let columnSums = []
+let diagonalSums = []
+
 
 const matrixify = function(userInputArr){
   matrix = userInputArr.reduce((rows, key, index) => (index % 3 == 0 ? rows.push([key]) 
   : rows[rows.length-1].push(key)) && rows, []);
   matrixSumRows(matrix);
+  matrixSumColumns(matrix);
   matrixSumDiagonals(matrix);
-   return console.log(sums);
+  console.log(`   ${matrix[0]} | ${rowSums[0]} \n   ${matrix[1]} | ${rowSums[1]} \n   ${matrix[2]} | ${rowSums[2]} \n-------------- \n${diagonalSums[0]} ${columnSums[0]} ${columnSums[1]} ${columnSums[2]} ${diagonalSums[1]}`)
 }
 
+const matrixSumRows = function(matrix){
+  return rowSums = matrix.map(r => r.reduce((a, b) => a + b));
+}
+const matrixSumColumns = function(matrix){
+  return columnSums = matrix.reduce((a, b) => a.map((x, i) => x + b[i]));
+}
 const matrixSumDiagonals = function(matrix) {
     let diagonal1 = 0, diagonal2 = 0;
     for (let row = 0; row < matrix.length; row++) {
         diagonal1 += matrix[row][row];
         diagonal2 += matrix[row][matrix.length - row - 1];
     }
-    return sums.push(diagonal1, diagonal2);
+    return diagonalSums.push(diagonal1, diagonal2);
 }
 
-
-const matrixSumRows = function(matrix){
-    return matrix.map(r => sums.push(r.reduce((a, b) => a + b)));
-    
- }
+function calcWinner() {
+  allSums = rowSums.concat(columnSums, diagonalSums);
+  return allEqual(allSums) ? console.log('Congratulations! You win!') : console.log('Boo, try again');
+}
 
 rl.question('Input a string of nine unique numbers ', (answer) => {
   // take string of numbers and turn into array of numbers
@@ -39,6 +48,7 @@ rl.question('Input a string of nine unique numbers ', (answer) => {
     userInput = answer.split('').map(Number);
     // turn into [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
     matrixify(userInput);
+    calcWinner();
     } else{
       console.log('Please enter a 9 digit string that uses the numbers 1-9 once each');
     }
